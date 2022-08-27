fcdb043c09fd144483fe5faf9844b16920070d4e
Nicolas Sauzede <nsauzede@laposte.net>
2022-08-27 21:03:07 +0200

    Factor common types header

--- src/common/types.h
+++ src/common/types.h
@@ -1,8 +1,14 @@
 #ifndef __TYPES_H__
 #define __TYPES_H__
 
+#ifdef _LP64
 typedef unsigned long uint64_t;
 typedef unsigned int uint32_t;
+#else
+typedef unsigned long uint32_t;
+typedef unsigned short uint16_t;
+#endif
+
 typedef unsigned short uint16_t;
 typedef unsigned char uint8_t;
 
