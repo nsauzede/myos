5fdc551a8eeb07464253dfd9630fcfbb2c2f94f1
Nicolas Sauzede <nsauzede@laposte.net>
2022-09-02 15:43:44 +0200

    Clean up whitespace; use only asm keyword

--- src/common/ioport.h
+++ src/common/ioport.h
@@ -1,17 +1,17 @@
 #define outb(value, port) \
-	__asm__ volatile (                                            \
+	asm volatile (                                          \
 	"outb %b0,%w1"                                          \
 	::"a" (value),"Nd" (port)                               \
 	)
 #define inb(port)                                               \
-	({                                                              \
-	unsigned char _v;                                             \
-	__asm__ volatile (                                            \
+	({                                                      \
+	unsigned char _v;                                       \
+	asm volatile (                                          \
 	"inb %w1,%0"                                            \
 	:"=a" (_v)                                              \
 	:"Nd" (port)                                            \
 	);                                                      \
-	_v;                                                           \
-	}) 
+	_v;                                                     \
+	})
 
 #define IODELAY() asm volatile( ".byte 0x90,0x90,0x90,0x90,0x90,0x90,0x90,0x90")
